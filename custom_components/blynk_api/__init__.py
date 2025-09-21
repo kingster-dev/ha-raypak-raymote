d612adfcb388f88c588de8330ba65ee7e5c15602
kingster-dev <andy@frizzip.com>
2025-09-21 17:10:11 -0500

    Update __init__.py
    adding options flow

--- custom_components/blynk_api/__init__.py
+++ custom_components/blynk_api/__init__.py
@@ -27,6 +27,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     # Store the API client for platforms to use
     hass.data[DOMAIN][entry.entry_id] = api_client
 
+    # Add an update listener to reload the integration when options change
+    entry.async_on_unload(entry.add_update_listener(update_listener))
+
     # Forward the setup to the platforms
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
@@ -43,3 +46,10 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
+
+
+async def update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Handle options update."""
+    _LOGGER.debug("Reloading integration due to options update")
+    await hass.config_entries.async_reload(entry.entry_id)
+
