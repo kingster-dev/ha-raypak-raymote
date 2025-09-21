06eb3744667710aba9f8410b7b6dd4d588204dcd
kingster-dev <andy@frizzip.com>
2025-09-21 17:12:33 -0500

    Update sensor.py
    adding options flow

--- custom_components/blynk_api/sensor.py
+++ custom_components/blynk_api/sensor.py
@@ -7,18 +7,10 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from .const import DOMAIN
+from .const import DOMAIN, CONF_SENSORS, CONF_PIN, CONF_NAME
 from .api import BlynkApiClient
 
 _LOGGER = logging.getLogger(__name__)
-
-# This is a sample list of sensors. In a more advanced version,
-# this would be configured via an options flow in the UI.
-Blynk_SENSORS = {
-    "V0": "Virtual Pin 0",
-    "V1": "Virtual Pin 1",
-    "V5": "Temperature",
-}
 
 SCAN_INTERVAL = timedelta(seconds=30)
 
@@ -27,12 +19,15 @@ async def async_setup_entry(
     entry: ConfigEntry,
     async_add_entities: AddEntitiesCallback,
 ) -> None:
-    """Set up the blynk sensors."""
+    """Set up the blynk sensors based on a config entry."""
     api_client: BlynkApiClient = hass.data[DOMAIN][entry.entry_id]
     
+    # Get sensors from options flow
+    configured_sensors = entry.options.get(CONF_SENSORS, [])
+    
     sensors = [
-        BlynkSensor(api_client, pin, name)
-        for pin, name in Blynk_SENSORS.items()
+        BlynkSensor(api_client, sensor_conf[CONF_PIN], sensor_conf[CONF_NAME])
+        for sensor_conf in configured_sensors
     ]
     async_add_entities(sensors, update_before_add=True)
 
@@ -44,9 +39,15 @@ class BlynkSensor(SensorEntity):
         """Initialize the sensor."""
         self._api = api_client
         self._pin = pin
-        self._attr_name = f"Blynk {name}"
-        self._attr_unique_id = f"blynk_sensor_{api_client._token[:6]}_{pin}"
+        self._attr_name = name
+        # Use the config entry's unique ID in the sensor's unique ID
+        self._attr_unique_id = f"{api_client._token[:6]}_sensor_{pin}"
         self._attr_native_value = None
+        self._attr_device_info = {
+            "identifiers": {(DOMAIN, api_client._token)},
+            "name": "Blynk Device",
+            "manufacturer": "Blynk",
+        }
 
     async def async_update(self) -> None:
         """Fetch new state data for the sensor."""
@@ -55,3 +56,4 @@ class BlynkSensor(SensorEntity):
             self._attr_native_value = value_array[0]
         else:
             self._attr_native_value = None
+
