249fd85293b9d9b7ba9b425c54db614f4cca1d37
kingster-dev <andy@frizzip.com>
2025-09-21 16:48:36 -0500

    Create const.py

---
+++ custom_components/blynk_api/const.py
@@ -0,0 +1,13 @@
+"""Constants for the Blynk API integration."""
+
+DOMAIN = "blynk_api"
+
+# Configuration keys
+CONF_SERVER = "server"
+CONF_TOKEN = "token"
+
+# Default values
+DEFAULT_SERVER = "https://blynk.cloud"
+
+# Platforms
+PLATFORMS = ["sensor", "switch"]
