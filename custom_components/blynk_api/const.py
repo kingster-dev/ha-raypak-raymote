2667dbbf3f9af6fe7a322177dba9317cb73ce7f2
kingster-dev <andy@frizzip.com>
2025-09-21 17:09:28 -0500

    Update const.py
    adding options flow

--- custom_components/blynk_api/const.py
+++ custom_components/blynk_api/const.py
@@ -5,9 +5,15 @@ DOMAIN = "blynk_api"
 # Configuration keys
 CONF_SERVER = "server"
 CONF_TOKEN = "token"
+CONF_SENSORS = "sensors"
+CONF_SWITCHES = "switches"
+CONF_PIN = "pin"
+CONF_NAME = "name"
+CONF_TYPE = "type"
 
 # Default values
 DEFAULT_SERVER = "https://blynk.cloud"
 
 # Platforms
 PLATFORMS = ["sensor", "switch"]
+
